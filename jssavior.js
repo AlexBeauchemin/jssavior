602f51c99b7700f48b265f51ec0d37466612d37a
Alex Beauchemin <alexbeauchemin01@gmail.com>
2014-04-22 10:55:37 -0400

    Rename
    Rename
    Avoid infinite loop on errors
    Added version
    Avoid console errors

---
+++ jssavior.js
@@ -0,0 +1,102 @@
+//Use tracekit to include complete stacktrace?
+(function($){
+  'use strict';
+  var JSSavior = {
+    version: 0.1,
+    serviceUrl: 'http://jssavior.com/api/send/',
+
+    init: function() {
+      var _this = this;
+
+      if (typeof console === "undefined") {
+        console = {
+          log: function (msg) { }
+        };
+      }
+
+      window.onerror = function(msg,file,line, column, errorObj) {
+        //Skip crossdomain error in old browsers if onerror is called from another domain
+        if (msg.indexOf('Script error.') > -1) {
+          return;
+        }
+
+        if (!JSSaviorConfig) {
+          console.log('JSSavior: You need to provide an account id');
+          return;
+        }
+
+        var data = {
+          domain: location.hostname,
+          file: file,
+          line: line,
+          message: msg,
+          project: JSSaviorConfig.projectId,
+          userAgent: navigator.userAgent,
+          version: _this.version
+        };
+
+        if (column) data.column = column;
+        if (errorObj && errorObj.message && errorObj.stack) data.stack = {message: errorObj.message, stack: errorObj.stack};
+
+        _this.postErrorData(data);
+
+        return true;
+      };
+    },
+
+    postErrorData: function(data) {
+      var _this = this;
+      if(!_this.serviceUrl) return;
+
+      //data.clientInfo = _this.getClientInfo();
+      data.clientInfo = {};
+
+      var xmlhttp;
+      if (window.XMLHttpRequest) xmlhttp = new XMLHttpRequest();
+      else xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+
+      xmlhttp.open("POST", _this.serviceUrl, true);
+      xmlhttp.setRequestHeader('Content-Type', 'application/json');
+
+      xmlhttp.onreadystatechange = function() {
+        _this.handleResult(xmlhttp);
+      };
+
+      xmlhttp.send(JSON.stringify(data));
+    },
+
+//    getClientInfo: function() {
+//      if(!JSON || !JSON.parse) return '';
+//      var xmlhttp;
+//      if (window.XMLHttpRequest) xmlhttp = new XMLHttpRequest();
+//      else xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+//
+//      xmlhttp.open("GET","http://api.hostip.info/get_json.php",false);
+//      xmlhttp.send();
+//
+//      return JSON.parse(xmlhttp.responseText);
+//    },
+
+    handleResult: function(xmlhttp) {
+      try {
+        if (xmlhttp.readyState == 4) {
+          if (xmlhttp.status === 200) {
+
+            if (!JSON || !JSON.parse || !xmlhttp.responseText) return null;
+
+            var result = JSON.parse(xmlhttp.responseText);
+            if (result.status == 'error') {
+              console.log('Javascript error cannot be logged: ' + result.reason);
+            }
+
+          } else {
+            console.log('JSSavior: Service unavailable');
+          }
+        }
+      }
+      catch (err) {}
+    }
+  };
+
+  JSSavior.init();
+})(window.jQuery);
